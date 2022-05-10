0d2c7088c92a6013537229428769ebafe1a31118
Rocio Arancibia <rooarancibia@gmail.com>
2022-05-10 20:23:40 -0300

    agregando funcion

--- algoritmo.js
+++ algoritmo.js
@@ -1,24 +1,31 @@
 //Programa que calcule el monto final de compra con IVA
 
 let nombre = prompt("¡Hola! Ingresa tu nombre");
-let opciones = Number(prompt("Hola " + nombre + 
-" a continuación selecciona alguna de las siguientes opciones"+
-        " 1. Calculadora de precio final con IVA"
-        +" 2. Salir"));
+let opciones = Number(
+  prompt(
+    "Hola " +
+      nombre +
+      " a continuación selecciona alguna de las siguientes opciones" +
+      " 1. Calculadora de precio final con IVA" +
+      " 2. Salir"
+  )
+);
 
-while (opciones !== 2)
-{
-  if (opciones==1)
-  { 
-    let precioConIva= Number(prompt("Coloca el monto de la compra"));
-IVA = 1.21;
-resultado = precioConIva*IVA;
-alert ("El monto total con IVA es de $ "+resultado)
-  }
-
-  if (opciones!==1&&
-  opciones!==2)
-{
-  alert("Opción incorrecta")
+//definicion de la funcion
+function resultadoMultiplicacion(a, b) {
+  resultado = a * b;
 }
+
+// ciclo
+while (opciones !== 2) {
+  if (opciones == 1) {
+    let precioConIva = Number(prompt("Coloca el monto de la compra"));
+    iva = 1.21;
+    resultadoMultiplicacion(precioConIva, iva);
+    alert("El resultado es $" + resultado);
+    alert = "¡Muchas gracias por tu visita!";
+  }
+  if (opciones !== 1 && opciones !== 2) {
+    alert("Opción incorrecta");
+  }
 }
